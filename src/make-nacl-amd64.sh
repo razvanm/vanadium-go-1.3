630dd026f9e1ea87f4ef06a200023f93cb78a18b
Jason Hickey <jyh@google.com>
2014-11-14 12:50:56 -0800

    Remove removed files.

--- src/make-nacl-amd64.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-(cd runtime/ppapi && ./mkzfile.py config.txt ppapi_nacl_amd64p32.st > zppapi_nacl_amd64p32.s)
-(cd runtime/ppapi && ./mkzfile.py --include=$NACL_SDK/pepper_35/include/ppapi/c config.txt cdecl_nacl.got > zcdecl_nacl.go)
-GOOS=nacl GOARCH=amd64p32 ./make.bash
