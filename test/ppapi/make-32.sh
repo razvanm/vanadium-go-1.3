fd8e4813154a2e94b905c86b629e79b9901df000
Jason Hickey <jyh@google.com>
2014-11-20 08:58:54 -0800

    MErge.

--- test/ppapi/make-32.sh
+++ test/ppapi/make-32.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x -e
 
-export GOROOT=${HOME}/google/nacl/go-ppapi-file
+export GOROOT=${HOME}/google/nacl/go-ppapi-network
 GOOS=nacl GOARCH=386 ${GOROOT}/bin/go build -o basic_x86_32.nexe basic_nacl.go
 GOOS=nacl GOARCH=386 ${GOROOT}/bin/go build -o mandel_x86_32.nexe mandel_nacl.go
 GOOS=nacl GOARCH=386 ${GOROOT}/bin/go build -o file_io_x86_32.nexe file_io_nacl.go
+GOOS=nacl GOARCH=386 ${GOROOT}/bin/go build -o network_x86_32.nexe network_nacl.go
--- test/ppapi/make-64.sh
+++ test/ppapi/make-64.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -x -e
 
-export GOROOT=${HOME}/google/nacl/go-ppapi-file
+export GOROOT=${HOME}/google/nacl/go-ppapi-network
 GOOS=nacl GOARCH=amd64p32 ${GOROOT}/bin/go build -o basic_x86_64.nexe basic_nacl.go
 GOOS=nacl GOARCH=amd64p32 ${GOROOT}/bin/go build -o mandel_x86_64.nexe mandel_nacl.go
 GOOS=nacl GOARCH=amd64p32 ${GOROOT}/bin/go build -o file_io_x86_64.nexe file_io_nacl.go
+GOOS=nacl GOARCH=amd64p32 ${GOROOT}/bin/go build -o network_x86_64.nexe network_nacl.go
