ff78d76d8d5847115483d1be82acd3162ed08d98
Jason Hickey <jyh@google.com>
2014-12-05 14:42:13 -0800

    Add exception test.

--- test/ppapi/basic_nacl.go
+++ test/ppapi/basic_nacl.go
@@ -7,15 +7,46 @@
 package main
 
 import (
+	"fmt"
 	"runtime/ppapi"
 )
+
+type junk struct {
+	a int
+	b, c interface{}
+}
+
+func (j *junk) GoString() string {
+	return fmt.Sprintf("junk{a: %d, b: %#v, c:%#v}", j.a, j.b, j.c)
+}
+
+func (j *junk) String() string {
+	return fmt.Sprintf("{a: %d, b: %v, c:%v}", j.a, j.b, j.c)
+}
+
+func make_junk(n int) *junk {
+	if n == 0 {
+		return nil
+	}
+	var value junk
+	value.a = n
+	value.b = make_junk(n - 1)
+	return &value
+}
+
+func (inst testInstance) run() {
+	v := make_junk(4)
+	inst.Printf("%#v\n", v)
+	inst.Printf("%v\n", v)
+}
 
 type testInstance struct{
 	ppapi.Instance
 }
 
 func (inst testInstance) DidCreate(args map[string]string) bool {
-	inst.Printf("DidCreate: %v", args)
+	inst.Printf("xDidCreate: %v", args)
+	inst.run()
 	return true
 }
 
